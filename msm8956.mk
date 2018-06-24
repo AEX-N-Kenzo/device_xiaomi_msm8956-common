f50fc90eb326ee4a9f15c63f160350982c5deced
Pzqqt <821026875@qq.com>
2018-06-24 16:24:44 +0800

    msm8956: Clean up
    Revert:
    db9bd7c61f7ed6940e5bc486f1f3ccadd949956f a48f237ff4c95eecd10e5de3f5ca5265d15cf627 a3a54b911a245192630b02c0643f9010acc0253d 6001fb98eedaaf64a260f1a7096815644c47a3a9 530d719c1cf0245d20ac3320c9dc8a086459460b 38dbe78c3b3c29aecff9d1067d87100547ade285

--- msm8956.mk
+++ msm8956.mk
@@ -125,7 +125,8 @@ PRODUCT_PACKAGES += \
     copybit.msm8952 \
     gralloc.msm8952 \
     hwcomposer.msm8952 \
-    memtrack.msm8952
+    memtrack.msm8952 \
+    liboverlay
 
 # Doze mode
 PRODUCT_PACKAGES += \
