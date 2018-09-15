d8b0d667f9ed87aff90bb4139e0e3184ebd5de5c
Pzqqt <821026875@qq.com>
2018-09-15 18:04:48 +0800

    toolchain: change to linaro 6.4.1

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -56,7 +56,7 @@ TARGET_KERNEL_APPEND_DTB := true
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8956
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := /home/pzqqt/bin/gcc-linaro-6.4.1-2018.04-rc1-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
 
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
